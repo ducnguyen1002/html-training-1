c70329d21a67ef6286d1b246dd3b2d4c6f69c5fe
ducnguyen1002 <ducduc1022000@gmail.com>
2023-08-16 22:27:06 +0700

    [html css] About page , done 2/3

---
+++ assets/js/function.js
@@ -0,0 +1,17 @@
+const btnScrollTop = document.querySelector("#scroll_to_top");
+const teamItem = document.querySelectorAll(".team_grid-item");
+const teamDialog = document.querySelector("#team_dialog");
+const teamDialogCloseBtn = document.querySelector("#dialog_close-btn");
+
+btnScrollTop.onclick = () => {
+  scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+teamItem.forEach((t) => {
+  t.addEventListener("click", () => {
+    teamDialog.showModal();
+  });
+});
+teamDialogCloseBtn.onclick = () => teamDialog.close();
