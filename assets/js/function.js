62bdaaca446bca8c3eb517ff48fdc32986d2dab1
ducnguyen1002 <ducduc1022000@gmail.com>
2023-08-17 19:30:37 +0700

    homework 17/8

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,7 +1,7 @@
 const btnScrollTop = document.querySelector("#scroll_to_top");
-const teamItem = document.querySelectorAll(".team_grid-item");
-const teamDialog = document.querySelector("#team_dialog");
-const teamDialogCloseBtn = document.querySelector("#dialog_close-btn");
+// const teamItem = document.querySelectorAll(".team_grid-item");
+// const teamDialog = document.querySelector("#team_dialog");
+// const teamDialogCloseBtn = document.querySelector("#dialog_close-btn");
 
 btnScrollTop.onclick = () => {
   scrollTo({
@@ -9,9 +9,9 @@ btnScrollTop.onclick = () => {
     behavior: "smooth",
   });
 };
-teamItem.forEach((t) => {
-  t.addEventListener("click", () => {
-    teamDialog.showModal();
-  });
-});
-teamDialogCloseBtn.onclick = () => teamDialog.close();
+// teamItem.forEach((t) => {
+//   t.addEventListener("click", () => {
+//     teamDialog.showModal();
+//   });
+// });
+// teamDialogCloseBtn.onclick = () => teamDialog.close();
